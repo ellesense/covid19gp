aa2ad698aeeb025e1ef8c28d8f5001a62d40e49f
Steve Kim <dkim.steve@gmail.com>
2020-11-20 20:31:05 -0800

    edited new user page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
 class User < ApplicationRecord
   has_many :entries
+
+  # validate :email_domain
+
+  # def email_domain
+  #   domain = email.split("@").last
+  #   if !email.blank?
+  #     errors.add(:email, "Invalid Domain") if domain != "mygreenplanet.com"
+  #   end
+  # end
+
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
