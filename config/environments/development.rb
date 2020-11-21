e3b3fb71368aa4353b82b5f069797c4e7e522315
Steve Kim <dkim.steve@gmail.com>
2020-11-20 22:26:47 -0800

    now sending sign up conf. emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,11 +31,22 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true 
 
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+     :address => "smtp.gmail.com",
+     :port => 587,
+     :domain => "gmail.com",
+     :authentication => 'plain',
+     :user_name => ENV["GMAIL_USERNAME"],
+     :password => ENV["GMAIL_PASSWORD"],
+     :enable_starttls_auto => true
+  }
+  
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -59,7 +70,4 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
-  # DEVISE CONFIGURATION
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
